89f98394dcfc35c7cfa620878257491abcc9b22f
DinosaurPower <lysym685@newschool.edu>
2022-04-09 16:37:34 -0400

    forgery

--- Jamegam dating sim/Assets/Counter.cs
+++ Jamegam dating sim/Assets/Counter.cs
@@ -44,6 +44,7 @@ public class Counter : MonoBehaviour
         dolphinCount = dolphinCount + dolphinNumber;
         carrotCount = carrotCount + carrotNumber;
 
+        GameObject.Find("STAT_MANAGER").GetComponent<StatManager>().UpdateCount();
 
     }
 }
